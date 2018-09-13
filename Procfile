c277e2e718cfe0d434295e0528fb0e6149e815e4
JanariKasar <janari.kasar@gmail.com>
2018-09-13 14:35:49 +0300

    Changed embedded derby DB to postgres

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/BurgerJoints-0.0.1-SNAPSHOT.jar
--- src/main/java/janari/burgers/BurgerJointsApplication.java
+++ src/main/java/janari/burgers/BurgerJointsApplication.java
@@ -1,13 +1,23 @@
 package janari.burgers;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @Controller
 @SpringBootApplication
 public class BurgerJointsApplication {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
 
 	@GetMapping("/")
 	String index() {
@@ -17,4 +27,16 @@ public class BurgerJointsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BurgerJointsApplication.class, args);
 	}
+
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		if (dbUrl == null || dbUrl.isEmpty()) {
+			return new HikariDataSource();
+		} else {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+		}
+	}
+
 }
